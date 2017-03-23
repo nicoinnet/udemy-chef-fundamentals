974e088ae3f1339bfd66dd7bce4562ac70cae4af
vagrant <vagrant@localhost.localdomain>
2017-03-23 23:57:13 +0000

    Added node attributes data generated by phai to message of the day setup recipe.

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -12,9 +12,18 @@ end
 
 package 'ntp'
 
+#ipaddress = node['ipaddress']
+#totalMemory = node['memory']['total']
+#hostname = node['hostname']
+
 file '/etc/motd' do
   action :create
-  content "This server is the property of Chef."
+  content "This server is the property of Chef.
+  HOSTNAME: #{node['hostname']}
+  IPADDRESS: #{node['ipaddress']}
+  CPU: #{node['cpu']['0']['mhz']}
+  MEMORY: #{node['memory']['total']}
+"
   owner 'root'
   group 'root'
   mode '0644'
