20c810d58d4b12e2490d34aa7b94dcb0b447f1a6
nicoinnet <nicoinnet@gmail.com>
2017-03-25 17:02:23 -0700

    apache cookbook: Added integration tests for apache cookbook.

--- cookbooks/apache/test/recipes/default_test.rb
+++ cookbooks/apache/test/recipes/default_test.rb
@@ -5,14 +5,11 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
+describe port(80) do
+  it { should be_listening }
+  its('protocols') { should include 'tcp6' }
 end
 
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe command("curl localhost | grep -o 'Hello World\!'") do
+  its(:stdout) { should match(/Hello World!/) }
 end
