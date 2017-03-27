80ec706dc4ff8594a1c8079143320a3bf97d6239
nicoinnet <nicoinnet@gmail.com>
2017-03-27 07:51:34 -0700

    apache cookbook: Added unit tests examples.

--- cookbooks/apache/spec/unit/recipes/default_spec.rb
+++ cookbooks/apache/spec/unit/recipes/default_spec.rb
@@ -9,12 +9,24 @@ require 'spec_helper'
 describe 'apache::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new({:platform => 'centos', :version => '7.2.1511'})
       runner.converge(described_recipe)
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'installs the correct package' do
+       expect(chef_run).to install_package('httpd')
+    end
+
+    it 'creates an default html file' do
+      expect(chef_run).to create_template('/var/www/html/index.html')
+    end
+
+    it 'starts the service' do
+      expect(chef_run).to start_service('httpd')
+    end
+
+    it 'enables the service' do
+      expect(chef_run).to enable_service('httpd')
     end
   end
 end
